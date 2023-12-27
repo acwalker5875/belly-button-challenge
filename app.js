dedf2f6edf03a2736f0878144aeec3dcdfca8787
acwalker5875 <137699333+acwalker5875@users.noreply.github.com>
2023-12-27 11:30:35 -0500

    updating file

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-// Use the D3 library to read in samples.json from the 
-// URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json
+// Fetch the JSON data and console log it
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+d3.json(url).then(function(data) {
+console.log(data);
+});
 
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-//     Use sample_values as the values for the bar chart.
-//     Use otu_ids as the labels for the bar chart.
+//     Use sample_values as the x values for the bar chart.
+//     Use otu_ids as the y labels for the bar chart.
 //     Use otu_labels as the hovertext for the chart.
 
+
+var dropdown = d3.select("#selDataset");
+    data.names.forEach(function(name) {
+    dropdown.append("option").text(name).property("value", name);
+    });
+
+let slicedData = sortedResults.slice(0, 10);
+
+let trace1 = {
+    x: slicedData.map(object => object.sampleValues),
+    y: slicedData.map(object => object.otu_ids),
+    text: slicedData.map(object => object.otu_labels),
+    name: "OTU's",
+    type: "bar",
+    orientation: "h"
+};
+
+  // Data array
+let data = [trace1];
+
+  // Apply a title to the layout
+let layout = {
+    title: "Top 10 OTUs for ${selectedSample}",
+    xaxis: { title: "Sample Values" },
+    yaxis: { title: "OTU IDs" },
+    margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+    }
+};
+
+  // Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
 
 // Create a bubble chart that displays each sample.
 //     Use otu_ids for the x values.
